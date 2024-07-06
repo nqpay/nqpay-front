e5ccf255244699d9112b57f0125bd10c9a34acc3
Valentin Vila <vilavalentin@gmail.com>
2024-07-06 02:00:06 -0300

    prevent firebase auth to fire email twice

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -83,7 +83,6 @@
 
             <button
               v-if="!signUpViewShow"
-              @click="login"
               type="submit"
               class="w-full text-white bg-[#BE38F3] focus:ring-4 focus:outline-none focus:ring-[#BE38F3] font-medium rounded-lg text-sm px-5 py-2.5 text-center"
             >
