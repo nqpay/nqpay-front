36428fe97cceea33770d81f5aa095ab104315af8
Valentin Vila <vilavalentin@gmail.com>
2024-07-21 19:37:12 -0300

    remove dark: from all components

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -58,12 +58,12 @@
       </div>
     </div>
     <div v-else class="flex flex-col items-center justify-center py-8 w-full px-10">
-      <a v-if="!emailSent" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+      <a v-if="!emailSent" class="flex items-center mb-6 text-2xl font-semibold text-white">
         <img class="w-16 h-16" src="/logo.png" alt="logo" />
       </a>
-      <div v-if="!emailSent" class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div v-if="!emailSent" class="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
         <div class="p-6 space-y-2">
-          <h1 class="pb-4 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Sign in to your account</h1>
+          <h1 class="pb-4 text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">Sign in to your account</h1>
 
           <button
             :disabled="!tyc_accepted"
@@ -127,13 +127,13 @@
           </div>
           <form class="space-y-4 md:space-y-6" v-on:submit.prevent="login">
             <div>
-              <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+              <label for="email" class="block mb-2 text-sm font-medium text-white">Email</label>
               <input
                 v-model="this.email"
                 type="email"
                 name="email"
                 id="email"
-                class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="border rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                 placeholder="Ingresa tu mail"
                 required=""
               />
@@ -144,7 +144,7 @@
                 <input v-model="tyc_accepted" type="checkbox" class="h-4 w-4 text-[#BE38F3] border-gray-300 rounded" />
               </div>
               <div class="ml-2 text-sm">
-                <label for="remember" class="text-gray-900 dark:text-white">
+                <label for="remember" class="text-white">
                   He leído y acepto los <a @click.prevent="showTyC = true" class="font-medium text-[#BE38F3] hover:underline cursor-pointer">términos y condiciones</a>
                 </label>
               </div>
--- src/components/CompleteProfile.vue
+++ src/components/CompleteProfile.vue
@@ -1,44 +1,44 @@
 <template>
   <section class="bg-[#1C1C1E] w-screen h-screen justify-center flex items-center">
     <div class="flex flex-col items-center justify-center py-8 w-full px-10">
-      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div class="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
         <div class="p-6 space-y-2">
-          <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">¿Eres mayor de edad?</h1>
-          <h2 class="text-gray-500 dark:text-gray-400 pb-4">Para continuar, necesitamos verificar tu edad.</h2>
+          <h1 class="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">¿Eres mayor de edad?</h1>
+          <h2 class="text-gray-400 pb-4">Para continuar, necesitamos verificar tu edad.</h2>
 
           <form class="space-y-4 md:space-y-6" v-on:submit.prevent="updateProfile">
             <div>
-              <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre Completo</label>
+              <label for="name" class="block mb-2 text-sm font-medium text-white">Nombre Completo</label>
               <input
                 v-model="this.name"
                 type="text"
                 name="name"
                 id="name"
-                class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="border rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                 placeholder="Ingresa tu nombre completo"
                 required=""
               />
             </div>
             <div>
-              <label for="birthdate" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fecha de Nacimiento</label>
+              <label for="birthdate" class="block mb-2 text-sm font-medium text-white">Fecha de Nacimiento</label>
               <input
                 v-model="this.birthdate"
                 type="date"
                 name="birthdate"
                 id="birthdate"
-                class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="border rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                 placeholder="Ingresa tu fecha de nacimiento"
                 required=""
               />
             </div>
             <div>
-              <label for="dni" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Documento Nacional de Identidad (DNI)</label>
+              <label for="dni" class="block mb-2 text-sm font-medium text-white">Documento Nacional de Identidad (DNI)</label>
               <input
                 v-model="this.dni"
                 type="text"
                 name="dni"
                 id="dni"
-                class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="border rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                 placeholder="Ingresa tu numero de DNI"
                 required=""
               />
@@ -51,11 +51,11 @@
               Confirmar
             </button>
 
-            <!-- <p v-if="!signUpViewShow" class="text-sm font-light text-gray-500 dark:text-gray-400">
+            <!-- <p v-if="!signUpViewShow" class="text-sm font-light text-gray-400">
               Don’t have an account yet?
               <a @click="signUpView" class="font-medium hover:underline text-[#BE38F3]">Sign up</a>
             </p> -->
-            <!-- <p v-else class="text-sm font-light text-gray-500 dark:text-gray-400">
+            <!-- <p v-else class="text-sm font-light text-gray-400">
               Already have an account yet?
               <a @click="signUpView" class="font-medium hover:underline text-[#BE38F3]">Sign in</a>
             </p> -->
