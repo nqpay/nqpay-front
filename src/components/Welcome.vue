2f67119dc16110ba332dba2f53b62fcd0b761b37
Valentin Vila <vilavalentin@gmail.com>
2024-07-21 19:21:28 -0300

    make Welcome view responsive

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -12,7 +12,7 @@
           y disfrutá de tu noche sin interrupciones
         </p>
         <router-link to="/auth">
-          <div class="w-20 h-20 bg-[#BE38F3] py-3 text-xl text-center rounded-full cursor-pointer flex items-center justify-center absolute bottom-20 right-16">
+          <div class="w-20 h-20 bg-[#BE38F3] py-3 text-xl text-center rounded-full cursor-pointer flex items-center justify-center absolute bottom-[5%] right-[5%]">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" class="w-10">
               <path style="fill: #ffffff" d="m17.5 5.999-.707.707 5.293 5.293H1v1h21.086l-5.294 5.295.707.707L24 12.499l-6.5-6.5z" data-name="Right" />
             </svg>
