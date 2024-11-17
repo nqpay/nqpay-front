8f0af94960886a518681dd2b263fad60af1b7531
Valentin Vila <vvila@Valentins-MacBook-Pro.local>
2024-11-16 21:20:37 -0300

    add return button on scanner

--- src/components/Scanner.vue
+++ src/components/Scanner.vue
@@ -1,14 +1,40 @@
 <template>
-  <div class="bg-[#F7F7FA] flex justify-center items-center w-screen h-screen text-black">
-    <template v-if="isAuthenticated">
-      <qrcode-stream @detect="onDetect"></qrcode-stream>
-    </template>
-    <template v-else>
-      <!-- Skeleton loader -->
-      <div class="animate-pulse">
-        <div class="w-64 h-64 bg-gray-300 rounded-lg"></div>
-      </div>
-    </template>
+  <div class="bg-[#F7F7FA] flex flex-col w-screen h-screen text-black relative">
+    <!-- Back button - Positioned at the top -->
+    <div class="absolute top-4 left-4 z-10">
+      <button 
+        @click="goBack"
+        class="flex items-center justify-center bg-white rounded-full p-3 shadow-lg hover:bg-gray-100 transition-colors"
+      >
+        <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          class="h-6 w-6" 
+          fill="none" 
+          viewBox="0 0 24 24" 
+          stroke="currentColor"
+        >
+          <path 
+            stroke-linecap="round" 
+            stroke-linejoin="round" 
+            stroke-width="2" 
+            d="M10 19l-7-7m0 0l7-7m-7 7h18" 
+          />
+        </svg>
+      </button>
+    </div>
+
+    <!-- Main content - Centered -->
+    <div class="flex-1 flex justify-center items-center">
+      <template v-if="isAuthenticated">
+        <qrcode-stream @detect="onDetect"></qrcode-stream>
+      </template>
+      <template v-else>
+        <!-- Skeleton loader -->
+        <div class="animate-pulse">
+          <div class="w-64 h-64 bg-gray-300 rounded-lg"></div>
+        </div>
+      </template>
+    </div>
   </div>
 </template>
 
@@ -58,6 +84,9 @@ export default {
       console.log('Detected ID:', id)
       this.$router.push(`/order/${id}`)
     },
+    goBack() {
+      this.$router.push('/admin')
+    }
   },
 }
 </script>
